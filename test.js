d735b420e0076f3a5ce8a681d712156bbce33fcb
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-28 22:53:08 +0700

    meta tweaks

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 import test from 'ava';
 import intoStream from 'into-stream';
-import fn from './';
+import m from './';
 
 test('buffer', async t => {
-	const data = await fn(new Buffer('name,val\nfoo,1\nbar,2'));
+	const data = await m(new Buffer('name,val\nfoo,1\nbar,2'));
 	t.is(data[0].name, 'foo');
 	t.is(data[1].name, 'bar');
 });
 
 test('string', async t => {
-	const data = await fn('name;val\nfoo;1\nbar;2', {separator: ';'});
+	const data = await m('name;val\nfoo;1\nbar;2', {separator: ';'});
 	t.is(data[0].name, 'foo');
 	t.is(data[1].name, 'bar');
 });
 
 test('stream', async t => {
-	const data = await fn(intoStream('name,val\nfoo,1\nbar,2'));
+	const data = await m(intoStream('name,val\nfoo,1\nbar,2'));
 	t.is(data[0].name, 'foo');
 	t.is(data[1].name, 'bar');
 });
 
 test('error', async t => {
-	t.throws(fn('name,val\nfoo,1,3\nbar,2', {strict: true}), /Row length does not match headers/);
+	t.throws(m('name,val\nfoo,1,3\nbar,2', {strict: true}), /Row length does not match headers/);
 });
