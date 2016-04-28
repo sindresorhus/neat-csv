81452b92dff6e5a4e2d5f09b8fefaf2bdcdebece
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-28 22:51:26 +0700

    switch to `get-stream`

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 'use strict';
 const intoStream = require('into-stream');
 const csvParser = require('csv-parser');
-const concatStream = require('concat-stream');
+const getStream = require('get-stream');
 
 module.exports = (input, opts) => {
-	opts = opts || {};
-
 	if (typeof input === 'string' || Buffer.isBuffer(input)) {
 		input = intoStream(input);
 	}
 
-	return new Promise((resolve, reject) => {
-		input
-			.pipe(csvParser(opts).on('error', reject))
-			.pipe(concatStream(resolve));
-	});
+	return getStream.array(input.pipe(csvParser(opts)));
 };
