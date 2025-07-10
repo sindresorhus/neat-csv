59bc85d9150d4a3fac27bfc88d22d7f0c038feb3
Daniel Han <daniel.han@scrive.com>
2025-07-10 10:53:12 +0200

    Make it support commonJS

---
+++ index.cjs
@@ -0,0 +1,6 @@
+'use strict';
+
+// This file is a CommonJS wrapper for the ESM module.
+module.exports = async (...args) =>
+	// eslint-disable-next-line node/no-unsupported-features/es-syntax
+	(await import('./index.js')).default(...args);
