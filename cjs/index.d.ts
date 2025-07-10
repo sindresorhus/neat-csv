62ca2b99b30cd64887669ac9b57ce1bcf9583a0a
Daniel Han <daniel.han@scrive.com>
2025-07-10 12:39:55 +0200

    Fix lint errors

--- cjs/index.d.ts
+++ cjs/index.d.ts
@@ -1,8 +1,7 @@
-// filepath: /Volumes/case-sensitive/tmp/neat-csv/index.d.ts
-// Note: This file should contain the CommonJS type declarations.
-// We use the .d.ts extension for compatibility with older TS versions,
-// but it will be mapped to the `require` export.
-import { Options as CsvParserOptions } from "csv-parser";
+import {Buffer} from 'node:buffer';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {Options as CsvParserOptions} from 'csv-parser';
 
 declare namespace neatCsv {
 	export interface Options extends CsvParserOptions {}
